2e41a84332505d7835f9cd2c2dc714b4c024d5a1
114253_Giacinto_Daniel <danielgiacinto@gmail.com>
2023-05-18 21:36:38 -0300

    agregar validacion
    Libreria de Jquery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,73 @@
+function limpiarCampos(){
+    document.getElementById('nombre').value = "";
+    document.getElementById('correo').value = "";
+    document.getElementById('asunto').value = "";
+    document.getElementById('mensaje').value = "";
+
+}
+
+// Validar con Jquery el formulario
+$(document).ready(function(){
+    $("#validar").validate({
+        rules: {
+            nombre: {
+                required: true,
+                minlength: 2
+            },
+            correo: {
+                required: true,
+                email: true
+            },
+            asunto: {
+                required: true
+            },
+            mensaje: {
+                required: true,
+                minlength: 2
+            },
+        },
+    
+        errorClass: "is-invalid",
+        validClass: "is-valid",
+    
+        submitHandler: function(form) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Exito',
+                text: 'Datos enviados correctamente',
+                timer: 2000
+            })
+            
+            limpiarCampos();
+        }
+       });
+})
+
+// Para traducir las validaciones
+
+jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo es obligatorio.",
+    remote: "Por favor, rellena este campo.",
+    email: "Por favor, escribe una dirección de correo válida",
+    url: "Por favor, escribe una URL válida.",
+    date: "Por favor, escribe una fecha válida.",
+    dateISO: "Por favor, escribe una fecha (ISO) válida.",
+    number: "Por favor, escribe un número entero válido.",
+    digits: "Por favor, escribe sólo dígitos.",
+    creditcard: "Por favor, escribe un número de tarjeta válido.",
+    equalTo: "Por favor, escribe el mismo valor de nuevo.",
+    accept: "Por favor, escribe un valor con una extensión aceptada.",
+    maxlength: jQuery.validator.format("Por favor, no escribas más de {0} caracteres."),
+    minlength: jQuery.validator.format("Por favor, no escribas menos de {0} caracteres."),
+    rangelength: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
+    range: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1}."),
+    max: jQuery.validator.format("Por favor, escribe un valor menor o igual a {0}."),
+    min: jQuery.validator.format("Por favor, escribe un valor mayor o igual a {0}.")
+  });
+
+
+
+
 let menuVisible = false;
 //Funcion que oculta o muestra el menu
 function mostrarOcultarMenu(){
